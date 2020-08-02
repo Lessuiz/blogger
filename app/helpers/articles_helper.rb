b78c0a83230fa370b51fe6279fcc7a8f0bbe2bd5
lessuiz <leonardoparreira2018@gmail.com>
2020-08-02 03:37:07 -0300

    Added a few gems

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def article_params
-    params.require(:article).permit(:body, :title, :tag_list)
+    params.require(:article).permit(:body, :title, :tag_list, :image)
   end
 end
